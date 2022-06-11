5d33740ce36a4ffc037c38f50eb0fb2f3927e9a0
bbobrek <bartosz.bobrek1993@gmail.com>
2022-06-11 15:52:58 +0200

    jee-spring-xml zadanka

--- jee-spring-xml/src/main/java/pl/coderslab/app/SpringDiApplication.java
+++ jee-spring-xml/src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -1,21 +1,11 @@
 package pl.coderslab.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.coderslab.beans.EmailService;
 import pl.coderslab.beans.HelloWorld;
+import pl.coderslab.beans.MessageSender;
+import pl.coderslab.beans.MessageService;
 
-/**
- * Utwórz klasę HelloWorld w pakiecie pl.coderslab.beans z atrybutem typu String oraz nazwie message.
- * Utwórz konstruktor ustawiający pole message, zdefiniuj gettery i settery.
- * Utwórz ziarno w konfiguracji xml o nazwie helloWorld klasy pl.coderslab.beans.HelloWorld.
- * Uzupełnij konfigurację ziaren, by przy pomocy konstruktora wstrzyknąć do klasy HelloWorld wartość Witaj Spring dla atrybutu message.
- * Uzupełnij klasę HelloWorld o poniższą metodę:
- * public void printMessage() {
- *       System.out.println("Your Message : " + message);
- * }
- *
- * Pobierz ziarno w metodzie main a następnie wywołaj na nim metodę printMessage.
- *
- * */
 public class SpringDiApplication {
 
     public static void main(String[] args) {
@@ -23,12 +13,22 @@ public class SpringDiApplication {
 
         HelloWorld helloWorld = ctx.getBean("helloWorld", HelloWorld.class);
         helloWorld.printMessage();
+        System.out.println(helloWorld.getMessage());
 
         HelloWorld helloWorld1 = (HelloWorld) ctx.getBean("helloWorld");
         helloWorld1.printMessage();
 
         HelloWorld helloWorld2 = ctx.getBean(HelloWorld.class);
         helloWorld2.printMessage();
+
+        System.out.println(helloWorld.getMessage());
+
+        MessageService messageService = (EmailService) ctx.getBean("emailService");
+        messageService.send();
+
+        MessageSender messageSender = ctx.getBean("messageSender", MessageSender.class);
+        messageSender.sendMessage();
+        messageSender.sendMessageFromProperty();
 
         ctx.close();
     }
