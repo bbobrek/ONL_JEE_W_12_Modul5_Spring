2bba286363c55fc39034023425341c51681e4993
bbobrek <bartosz.bobrek1993@gmail.com>
2022-06-12 14:36:57 +0200

    jee-spring-mvc views zad1/2

--- jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,7 +1,10 @@
 package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalTime;
 
 @Controller
 public class HelloController {
@@ -16,6 +19,21 @@ public class HelloController {
     @ResponseBody
     public String helloWorld() {
         return "Hello world";
+    }
+
+    @GetMapping("/helloView")
+    public String helloView(Model model) {
+        LocalTime actualTime = LocalTime.now();
+        if (actualTime.isAfter(LocalTime.of(8, 0)) &&
+                actualTime.isBefore(LocalTime.of(20, 0))) {
+            model.addAttribute("color", "black");
+            model.addAttribute("backgroundColor", "white");
+        } else {
+            model.addAttribute("color", "white");
+            model.addAttribute("backgroundColor", "black");
+        }
+        return "home";
+        // dzieki konfiguracji bedzie /WEB-INF/views/home.jsp
     }
 
 }
---
+++ jee-spring-mvc/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Bartek
+  Date: 12.06.2022
+  Time: 14:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <style>
+        body {
+            color: ${color};
+            background-color: ${backgroundColor};
+        }
+    </style>
+</head>
+<body>
+  <h1> Witaj w widoku ;) </h1>
+</body>
+</html>
