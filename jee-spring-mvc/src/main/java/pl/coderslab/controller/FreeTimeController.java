c8b7c9a5246a76f482b1f806728b3f7385939678
bbobrek <bartosz.bobrek1993@gmail.com>
2022-06-14 23:22:18 +0200

    logger

--- jee-spring-mvc/src/main/java/pl/coderslab/controller/FreeTimeController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/FreeTimeController.java
@@ -1,5 +1,7 @@
 package pl.coderslab.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,19 +12,25 @@ import java.time.LocalTime;
 @Controller
 public class FreeTimeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(FreeTimeController.class);
     private static final LocalTime START_WORK = LocalTime.of(9,0);
     private static final LocalTime END_WORK = LocalTime.of(17,0);
 
     @GetMapping("/checkTime")
     @ResponseBody
     public String checkTime() {
+        logger.debug("Started method to check time");
         LocalDateTime localDateTime = LocalDateTime.now();
+        logger.debug("Checking free time for {}", localDateTime);
         if (DayOfWeek.SATURDAY.equals(localDateTime.getDayOfWeek())
                 || DayOfWeek.SUNDAY.equals(localDateTime.getDayOfWeek())) {
+            logger.debug("Given day is day off: {}", localDateTime.getDayOfWeek());
             return "Wolne";
         } else if (localDateTime.toLocalTime().isAfter(START_WORK) && localDateTime.toLocalTime().isBefore(END_WORK)) {
+            logger.debug("working hours, do not disturb: {}", localDateTime.toLocalTime());
             return "Pracuje, nie dzwon";
         } else {
+            logger.debug("working day but not working hours, {}", localDateTime.toLocalTime());
             return "Po pracy";
         }
     }
--- jee-spring-mvc/src/main/java/pl/coderslab/controller/session/CartController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/session/CartController.java
@@ -1,5 +1,7 @@
 package pl.coderslab.controller.session;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CartController {
+
+    private final static Logger logger = LoggerFactory.getLogger(CartController.class);
 
     private Cart cart;
     private ProductDao productDao;
@@ -28,12 +32,15 @@ public class CartController {
     @GetMapping("/addtocart")
     @ResponseBody
     public String addToCart(@RequestParam("id") long id, @RequestParam("quantity") int quantity) {
+        logger.debug("running add to cart with given params, id: {}, quantity: {}", id, quantity);
         if(cart.hasProductWithId(id)) {
+            logger.debug("there is already product with given id, updating");
             cart.updateQuantity(id, quantity);
             return "quantityUpdated";
         }
         Product product = productDao.getProductById(id);
         CartItem cartItem = new CartItem(quantity, product);
+        logger.debug("This is new product in cart, adding new cart item: {}", cartItem);
         cart.addToCart(cartItem);
         return "addtocart";
     }
